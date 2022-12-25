4e3ae58857a10525107dd404d047392e11fc5285
Rodrigo Salgado <rodrigosdc20.ma@hotmail.com>
2022-12-25 02:51:04 -0300

    Initial commit

---
+++ config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+class DB {
+    private $host = '';
+    private $user = '';
+    private $password = '';
+    private $database = '';
+
+    public function __construct() {
+        $this->host = getenv('DB_HOST');
+        $this->user = getenv('DB_USER');
+        $this->password = getenv('DB_PASSWORD');
+        $this->database = getenv('DB_NAME');
+    }
+
+    public function connect() {
+        $mysql_connect_str = "mysql:host=$this->host;dbname=$this->database";
+        $dbConnection = new PDO($mysql_connect_str, $this->user, $this->password);
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    }
+}
---
+++ routes/car.php
@@ -0,0 +1,153 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+// Obter todos os carros
+$app->get("/cars/all", function (
+    Request $request,
+    Response $response,
+    array $args
+) {
+    $sql = "SELECT * FROM tbl_car";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+
+        $stmt = $conn->query($sql);
+        $cars = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+
+        return $response->withJson($cars, 200);
+    } catch (PDOException $e) {
+        $error = [
+            "status" => "failed",
+            "message" => $e->getMessage(),
+        ];
+        return $response->withJson($error, 500);
+    }
+});
+
+// Obter um unico carro pelo id
+$app->get("/cars/{id}", function (
+    Request $request,
+    Response $response,
+    array $args
+) {
+    $id = $args["id"];
+    $sql = "SELECT * FROM tbl_car WHERE id = $id";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+
+        $stmt = $conn->query($sql);
+        $car = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+
+        return $response->withJson($car, 200);
+    } catch (PDOException $e) {
+        $error = [
+            "status" => "failed",
+            "message" => $e->getMessage(),
+        ];
+        return $response->withJson($error, 500);
+    }
+});
+
+// Adicionar um carro
+$app->post("/cars/add", function (
+    Request $request,
+    Response $response,
+    array $args
+) {
+    $data = $request->getParsedBody();
+    $sql =
+        "INSERT INTO tbl_car (brand_model, license_plate, tbl_owner_id) VALUES (:brand_model, :license_plate, :tbl_owner_id)";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":brand_model", $data["brand_model"]);
+        $stmt->bindParam(":license_plate", $data["license_plate"]);
+        $stmt->bindParam(":tbl_owner_id", $data["tbl_owner_id"]);
+        $stmt->execute();
+
+        $data["id"] = $conn->lastInsertId();
+        $db = null;
+
+        return $response->withJson($data, 200);
+    } catch (PDOException $e) {
+        $error = [
+            "status" => "failed",
+            "message" => $e->getMessage(),
+        ];
+        return $response->withJson($error, 500);
+    }
+});
+
+// Atualizar um carro
+$app->put("/cars/update/{id}", function (
+    Request $request,
+    Response $response,
+    array $args
+) {
+    $id = $args["id"];
+    $data = $request->getParsedBody();
+    $sql = "UPDATE tbl_car SET brand_model = :brand_model, license_plate = :license_plate, tbl_owner_id = :tbl_owner_id WHERE id = $id";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":brand_model", $data["brand_model"]);
+        $stmt->bindParam(":license_plate", $data["license_plate"]);
+        $stmt->bindParam(":tbl_owner_id", $data["tbl_owner_id"]);
+        $stmt->execute();
+
+        $db = null;
+
+        return $response->withJson($data, 200);
+    } catch (PDOException $e) {
+        $error = [
+            "status" => "failed",
+            "message" => $e->getMessage(),
+        ];
+        return $response->withJson($error, 500);
+    }
+});
+
+// Deletar um carro
+$app->delete("/cars/delete/{id}", function (
+    Request $request,
+    Response $response,
+    array $args
+) {
+    $id = $args["id"];
+    $sql = "DELETE FROM tbl_car WHERE id = $id";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $db = null;
+
+        return $response->withJson("Carro deletado com sucesso", 200);
+    } catch (PDOException $e) {
+        $error = [
+            "status" => "failed",
+            "message" => $e->getMessage(),
+        ];
+        return $response->withJson($error, 500);
+    }
+});
